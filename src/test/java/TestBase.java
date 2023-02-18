72a629e73abe59de4e9fd5e9b0c970f41bbfe27a
Conjuction <suhinin-d@mail.ru>
2023-02-18 19:32:34 +0500

    add HW

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,10 +3,9 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TestBase {
-    protected final static String URL = "https://github.com/";
-    protected final String REPOSITORY = "selenide/selenide";
-    protected final SelenideElement inputSearch = $(".header-search-input");
-    protected final SelenideElement issuesTab = $("#issues-tab");
-
-    protected final String ISSUE_NUMBER = "#2152";
+    public final static String URL = "https://github.com/";
+    public final String REPOSITORY = "selenide/selenide";
+    public final SelenideElement inputSearch = $(".header-search-input");
+    public final SelenideElement issuesTab = $("#issues-tab");
+    public final String ISSUE_NUMBER = "#2152";
 }
